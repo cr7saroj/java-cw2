6f1421c9518fece69f4d3fb7daef0f8b12028531
cr7saroj <sarojrasaily16@gmail.com>
2024-03-19 18:05:29 +0000

    Added all files for cw2

---
+++ Task1/src/task1/ApplicationRunner.java
@@ -0,0 +1,15 @@
+
+package task1;
+
+import java.io.File;
+
+public class ApplicationRunner {
+
+
+    public static void main(String[] args) {
+        
+        String fileLocation = System.getProperty("user.dir");
+        String dataPath = fileLocation + File.separator + "literature-prizes.txt";
+    }
+    
+}
---
+++ Task2/src/task2/ApplicationRunner.java
@@ -0,0 +1,10 @@
+
+package task2;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
